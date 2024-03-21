c977330b078129a7e019cf4a78d9ee6a0d6c245f
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-03-21 20:52:15 +0200

    Department Controller - Dependency Injection

---
+++ AppMVC.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using AppMVC.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppMVC.PL.Controllers
+{
+	// Inhertiance : DepartmentController is Controller
+	// Composition : DepartmentController has a DepartmentRepository
+	public class DepartmentController : Controller
+	{
+		private readonly IDepartmentRepository _departmentRepo; // NULL
+
+        public DepartmentController(IDepartmentRepository departmentRepo)
+        {
+			_departmentRepo = departmentRepo;
+		}
+
+        // /Department/Index
+        public IActionResult Index()
+		{
+			//var department = _departmentRepo.GetAllDepartment();
+
+			return View();
+		}
+	}
+}
