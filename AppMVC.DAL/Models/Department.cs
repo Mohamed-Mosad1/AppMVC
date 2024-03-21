f7c697b8cef7e70df2bb17877decb123142ea8de
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-03-21 21:40:31 +0200

    Department Controller - Index

--- AppMVC.DAL/Models/Department.cs
+++ AppMVC.DAL/Models/Department.cs
@@ -11,8 +11,14 @@ namespace AppMVC.DAL.Models
 	public class Department
 	{
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Code Is Required!!")]
         public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name Is Required!!")]
         public string Name { get; set; }
+
+        [Display(Name = "Date Of Creation")]
         public DateTime DateOfCreation { get; set; }
     }
 }
