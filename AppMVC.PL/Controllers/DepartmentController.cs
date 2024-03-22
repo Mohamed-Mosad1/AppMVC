e990ff48a4413df64d71a1245ba9c2539519f14c
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-03-23 00:26:40 +0200

    Department Controller - Edit

--- AppMVC.PL/Controllers/DepartmentController.cs
+++ AppMVC.PL/Controllers/DepartmentController.cs
@@ -1,62 +1,115 @@
 ﻿using AppMVC.BLL.Interfaces;
 using AppMVC.DAL.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace AppMVC.PL.Controllers
 {
-	// Inhertiance : DepartmentController is Controller
-	// Composition : DepartmentController has a DepartmentRepository
-	public class DepartmentController : Controller
-	{
-		private readonly IDepartmentRepository _departmentRepo; // NULL
+    // Inhertiance : DepartmentController is Controller
+    // Composition : DepartmentController has a DepartmentRepository
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepo; // NULL
+        private readonly IWebHostEnvironment _env;
 
-        public DepartmentController(IDepartmentRepository departmentRepo)
+        public DepartmentController(IDepartmentRepository departmentRepo, IWebHostEnvironment env)
         {
-			_departmentRepo = departmentRepo;
-		}
+            _departmentRepo = departmentRepo;
+            _env = env;
+        }
 
         // /Department/Index
         public IActionResult Index()
-		{
-			var department = _departmentRepo.GetAllDepartment();
+        {
+            var department = _departmentRepo.GetAllDepartment();
 
-			return View(department);
-		}
+            return View(department);
+        }
 
-		
-		public IActionResult Create()
-		{
-			return View();
-		}
 
-		[HttpPost]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Create(Department department)
         {
-            if(ModelState.IsValid) // Server Side Validation
-			{
-				var count = _departmentRepo.AddDepartment(department);
-				if(count > 0)
-				{
-					return RedirectToAction(nameof(Index));
-				}
-			}
-			return View(department);
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                var count = _departmentRepo.AddDepartment(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(department);
         }
 
-		public IActionResult Details(int? id)
-		{
+        public IActionResult Details(int? id, string viewName = "Details")
+        {
             if (!id.HasValue)
             {
-				return BadRequest(); // 400
+                return BadRequest(); // 400
             }
 
-			var department = _departmentRepo.GetDepartmentById(id.Value);
+            var department = _departmentRepo.GetDepartmentById(id.Value);
 
-			if(department == null)
-			{
-				return NotFound(); // 404
-			}
-			return View(department);
+            if (department == null)
+            {
+                return NotFound(); // 404
+            }
+            return View(viewName, department);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            ///if (!id.HasValue)
+            ///{
+            ///    return BadRequest(); // 400
+            ///}
+            ///var department = _departmentRepo.GetDepartmentById(id.Value);
+            ///if (department == null)
+            ///{
+            ///    return NotFound(); // 404
+            ///}
+            ///return View(department);
+
+            return Details(id, "Edit");
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int id,Department department)
+        {
+            if (id != department.Id)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(department);
+            }
+
+            try
+            {
+                _departmentRepo.UpdateDepartment(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                // 1. Log Exception
+                // 2. Friendly Message
+                if (_env.IsDevelopment())
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                else
+                    ModelState.AddModelError(string.Empty, "An Error Has Occurred during Updating the Department");
+
+                return View(department);
+            }
+        }
+
     }
 }
