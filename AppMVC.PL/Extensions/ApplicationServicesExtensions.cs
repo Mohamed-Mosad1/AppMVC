6a92c486b4dde46ed6ee9a892fd73f14c35d9222
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-03-26 20:55:46 +0200

    AddSingletone Vs AddScoped vs AddTransient

---
+++ AppMVC.PL/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using AppMVC.BLL.Interfaces;
+using AppMVC.BLL.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppMVC.PL.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            return services;
+        }
+    }
+}
--- AppMVC.PL/Startup.cs
+++ AppMVC.PL/Startup.cs
@@ -1,6 +1,7 @@
 using AppMVC.BLL.Interfaces;
 using AppMVC.BLL.Repositories;
 using AppMVC.DAL.Data;
+using AppMVC.PL.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,12 +33,11 @@ namespace AppMVC.PL
 			services.AddDbContext<ApplicationDbContext>(options =>
 			{
 				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-			});
+			}, ServiceLifetime.Scoped);
 
-			services.AddScoped<IDepartmentRepository, DepartmentRepository>();
-			services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+			services.AddApplicationServices(); // Extension Method
 
-		}
+        }
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
