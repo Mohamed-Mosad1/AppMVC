e046ed27aa354843378cabe0ec80d241eb46f8bb
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-04-03 02:40:29 +0200

    Account Controller - SignUp

---
+++ AppMVC.PL/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using AppMVC.DAL.Models;
+using AppMVC.PL.ViewModels.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AppMVC.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        #region Sign Up
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user is null)
+                {
+                    user = new ApplicationUser()
+                    {
+                        Fname = model.FirstName,
+                        Lname = model.LastName,
+                        UserName = model.UserName,
+                        Email = model.Email,
+                        IsAgree = model.IsAgree
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+
+                    if (result.Succeeded)
+                        return RedirectToAction(nameof(SignIn));
+
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+
+                ModelState.AddModelError(string.Empty, "This username is already in use for another account");
+            }
+
+            return View(model);
+        }
+
+        #endregion
+
+        #region Sign In
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
---
+++ AppMVC.PL/ViewModels/User/SignUpViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppMVC.PL.ViewModels.User
+{
+	public class SignUpViewModel
+	{
+		[Required(ErrorMessage = "User Name is Required")]
+		public string UserName { get; set; }
+
+		[Required(ErrorMessage = "Email is Required")]
+		[EmailAddress(ErrorMessage = "Invalid Email")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "First Name is Required")]
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; }
+
+		[Required(ErrorMessage = "Last Name is Required")]
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
+
+		[Required(ErrorMessage = "Password is Required")]
+		[MinLength(5, ErrorMessage = "Minimum Password Length is 5 ")]
+		[DataType(DataType.Password)]
+        public string Password { get; set; }
+
+		[Required(ErrorMessage = "Confirm Password is Required")]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password), ErrorMessage = "Confirm Password does not match with Password")]
+		public string ConfirmPassword { get; set; }
+
+        public bool IsAgree { get; set; }
+
+
+    }
+}
