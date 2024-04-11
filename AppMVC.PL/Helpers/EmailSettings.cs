920e3e493e5710a7c4bf108a5c61bb39e670c9f1
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-04-11 21:29:31 +0200

    Send Email

--- AppMVC.PL/Helpers/EmailSettings.cs
+++ AppMVC.PL/Helpers/EmailSettings.cs
@@ -1,4 +1,6 @@
 ﻿using AppMVC.DAL.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace AppMVC.PL.Helpers
 {
@@ -6,7 +8,15 @@ namespace AppMVC.PL.Helpers
     {
         public static void SendEmail(Email email)
         {
+            // Email Server : Gmail.com
 
+            var client = new SmtpClient("smtp.gmail.com", 587);
+
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential("mohamedmosaad115@gmail.com", "dvtcsttfyiabcbtz");
+
+            client.Send("mohamedmosaad115@gmail.com", email.Recipients, email.Subject, email.Body); // Send Email
         }
     }
 }
