a870d45b6a1fb1e7c0883b5dcf2b43445c2ee9e3
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-04-11 13:28:00 +0200

    AccountController - SignIn

--- AppMVC.PL/Controllers/AccountController.cs
+++ AppMVC.PL/Controllers/AccountController.cs
@@ -62,10 +62,36 @@ namespace AppMVC.PL.Controllers
         #endregion
 
         #region Sign In
-
+        [HttpGet]
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user is not null)
+                {
+                    var flag = await _userManager.CheckPasswordAsync(user, model.Password);
+
+                    if (flag)
+                    {
+                        var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+
+                        if (result.Succeeded)
+                            return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login!");
+            }
+
+            return View(model);
         }
 
         #endregion
